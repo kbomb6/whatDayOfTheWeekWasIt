3a3b0475317bbb1cbf65369390e4830e5d7dfab4
kbomb6 <baumanfamily4@gmail.com>
2018-11-19 12:25:29 -0700

    Initial commit

---
+++ whatDayOfTheWeekWasIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace whatDayOfTheWeekWasIt
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            WhatDayOfTheWeekWasIt newDay = new WhatDayOfTheWeekWasIt();
+            newDay.ReturnDay();
+        }
+    }
+}
---
+++ whatDayOfTheWeekWasIt/WhatsTheDayOfTheWeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace whatDayOfTheWeekWasIt
+{
+    public class WhatDayOfTheWeekWasIt
+    {
+        public void ReturnDay()
+        {
+            Console.WriteLine("Enter a date and I'll tell you what day of the week it was!");
+            var userInput = Console.ReadLine();
+            DateTime result;
+            DateTime.TryParse(userInput, out result);
+
+            if (!result.Equals(DateTime.MinValue))
+            {
+                Console.WriteLine(result.DayOfWeek.ToString());
+            }
+            else {
+                Console.WriteLine("Oops! Not a valid date. Try again please.");
+            }
+        }
+    }
+}
